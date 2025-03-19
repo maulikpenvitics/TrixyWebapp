d2fff63a47b51e9860b9a6795d88b2b670911755
Dabhi Varsha <varshad.envitics@gmail.com>
2025-03-19 16:03:05 +0700

    comment insert data in historical data

--- TrixyWebapp/Controllers/HomeController.cs
+++ TrixyWebapp/Controllers/HomeController.cs
@@ -68,8 +68,8 @@ namespace TrixyWebapp.Controllers
                 //NSE:RELIANCE-EQ
                 //NSE: BAJFINANCE - EQ
                 //NSE:ABFRL-EQ
-                var data = await _fyersWebSocket.FetchAndStoreHistoricalStockDataAsync("NSE:VEDL-EQ", DateTime.UtcNow.AddDays(-30).ToString("yyyy-MM-dd"), DateTime.UtcNow.ToString("yyyy-MM-dd"));
-                await _HistoricalStockdata.InsertManyAsync(data);
+                //var data = await _fyersWebSocket.FetchAndStoreHistoricalStockDataAsync("NSE:VEDL-EQ", DateTime.UtcNow.AddDays(-30).ToString("yyyy-MM-dd"), DateTime.UtcNow.ToString("yyyy-MM-dd"));
+                //await _HistoricalStockdata.InsertManyAsync(data);
                 var gethistoricaldata = await _HistoricalStockdata.GetAllAsync();
 
                 //var gethistoricaldata = await _stockRepository.GetStockDataBySymbolAsync("NSE:OFSS - EQ");
